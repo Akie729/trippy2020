b3834abbaa54df6624ea2a1f7e83f202c9475811
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-12-03 18:51:36 +0900

    Revert "komitto"
    This reverts commit 3ea63844acb5477ac7fe4ef6c06fe724cf1a3e08.

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,23 +1,25 @@
-$(document).on('turbolinks:load', function() {
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
 
-  $('#trip_images').on('change',function(e){
-    var files = e.target.files;
-    var d = (new $.Deferred()).resolve();
-    $.each(files,function(i,file){
-      d = d.then(function(){return previewImage(file)});
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+     const imageElement = document.createElement('div');
+
+     // 表示する画像を生成
+     const blobImage = document.createElement('img');
+     blobImage.setAttribute('src', blob);
+     // 生成したHTMLの要素をブラウザに表示させる
+     imageElement.appendChild(blobImage);
+     ImageList.appendChild(imageElement);
+   };
+
+    document.getElementById('trip_images').addEventListener('change', function(e){
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+    
+      createImageHTML(blob);
     });
-  })
-
-  var previewImage = function(imageFile){
-    var reader = new FileReader();
-    var img = new Image();
-    var def =$.Deferred();
-    reader.onload = function(e){
-      $('.images_field').append(img);
-      img.src = e.target.result;
-      def.resolve(img);
-    };
-    reader.readAsDataURL(imageFile);
-    return def.promise();
-  }
-})
+  });
+}
