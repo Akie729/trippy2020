8ff0bd0c027dbe8304663e76828b9ba5fe490bce
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-12-05 11:50:11 +0900

    アソシエーション記載

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :trip_tag_relations
+  has_many :trips, through: :trip_tag_relations
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,8 @@
 class Trip < ApplicationRecord
   has_many_attached :images
   belongs_to :user, optional: true
+  has_many :trip_tag_relations
+  has_many :tags, through: :trip_tag_relations
 
   with_options presence: true do
     validates :images
